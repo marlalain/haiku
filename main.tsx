e5b138729f6efd4b5744f02c130aa219f88a962e
Alex <alex@alexaa.dev>
2022-10-13 05:57:09 -0300

    reformat(main): removes dead code

--- main.tsx
+++ main.tsx
@@ -31,16 +31,4 @@ blog({
             url: "https://alexaa.dev"
         }
     ],
-    middlewares: [
-
-        // If you want to set up Google Analytics, paste your GA key here.
-        // ga("UA-XXXXXXXX-X"),
-
-        // If you want to provide some redirections, you can specify them here,
-        // pathname specified in a key will redirect to pathname in the value.
-        redirects({
-            "/hello_world.html": "/hello_world",
-        }),
-
-    ]
 });
