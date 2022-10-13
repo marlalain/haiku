c97a45dcb773aff51e0d56f60b7f37032ac5cf4e
Alex <alex@alexaa.dev>
2022-10-13 05:35:49 -0300

    chore: configures blog

--- main.tsx
+++ main.tsx
@@ -1,27 +1,46 @@
 /** @jsx h */
 
-import blog, { ga, redirects, h } from "blog";
+import blog, {redirects, h} from "blog";
 
 blog({
-  title: "My Blog",
-  description: "This is my new blog.",
-  // header: <header>Your custom header</header>,
-  // section: <section>Your custom section</section>,
-  // footer: <footer>Your custom footer</footer>,
-  avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
-  avatarClass: "rounded-full",
-  author: "An author",
+    title: "alex's haikus",
+    description: "haikus about tech and stuff",
+    showHeaderOnPostPage: true,
+    // header: <header>Your custom header</header>,
+    // section: <section>Your custom section</section>,
+    // footer: <footer>Your custom footer</footer>,
+    avatar: "https://cdn.upload.systems/uploads/EWwItQHa.jpg",
+    avatarClass: "rounded-full",
+    author: "Alex",
+    lang: "en",
+    links: [
+        {
+            title: "Email",
+            url: "mailto:haikus@alexaa.dev"
+        },
+        {
+            title: "GitHub",
+            url: "https://github.com/minsk-dev"
+        },
+        {
+            title: "Twitter",
+            url: "https://twitter.com/alexsyntaxtree"
+        },
+        {
+            title: "Curriculum",
+            url: "https://alexaa.dev"
+        }
+    ],
+    middlewares: [
 
-  // middlewares: [
+        // If you want to set up Google Analytics, paste your GA key here.
+        // ga("UA-XXXXXXXX-X"),
 
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
+        // If you want to provide some redirections, you can specify them here,
+        // pathname specified in a key will redirect to pathname in the value.
+        redirects({
+            "/hello_world.html": "/hello_world",
+        }),
 
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
-
-  // ]
+    ]
 });
